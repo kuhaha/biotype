aed2d54bcf8da5bcf7b7e67ce847e0b269369c09
kuhaha <kevin.yr.cheng@gmail.com>
2023-01-07 13:03:06 +0900

    Add weighted similarity

--- similarity.py
+++ similarity.py
@@ -1,38 +1,43 @@
 # 類似度計算
 import math
 
-def jaccard_similarity(pk1, pk2):
-    npk1, npk2 = _peak(pk1, d=1), _peak(pk2, d=1)
+def jaccard_similarity(pk1, pk2, d=1):
+    npk1, npk2 = _peak(pk1, g=d), _peak(pk2, g=d)
     common = npk1.keys() & npk2.keys()
     union  = npk1.keys() | npk2.keys()
 
     # n_common: # of co-occurent peaks
     n_common, n_union = len(common), len(union)
-
     return n_common / float(n_union)
 
-def rank_similarity(pk1, pk2, good_with=5):
-    npk1, npk2 = _peak(pk1, d=1), _peak(pk2, d=1)
+def rank_similarity(pk1, pk2, d=1, good_with=2, weighted=False):
+    npk1, npk2 = _peak(pk1, g=d), _peak(pk2, g=d)
     common = npk1.keys() & npk2.keys()
     union  = npk1.keys() | npk2.keys()
     
-    # n_common = # of co-occurrent "good" peaks whose ranks are close enough 
+    # n_common = # of co-occurrent "good" peaks  
     rpk1, rpk2 = _rank(npk1), _rank(npk2)
     good_peak = lambda x: abs(rpk1[x]-rpk2[x]) < good_with
-    good_common = [good_peak(k) for k in common]
-    n_common, n_union = sum(good_common), len(union)
-    
-    return n_common / float(n_union)
+    if weighted:
+        weight = lambda r: 1/float(r) # Reciprocal rank
+        good_weighted = [0.5+weight(rpk1[k])+weight(rpk2[k]) for k in common if good_peak(k)] 
+#         n1,n2 = len(rpk1), len(rpk2)
+#         weight = lambda r, n: 1-(r-1)/float(n) # Rank sum
+#         good_weighted = [0.5+weight(rpk1[k],n1)+weight(rpk2[k],n2) for k in common if good_peak(k)] 
+        return sum(good_weighted)/len(union)
+    else:
+        good_common = [good_peak(k) for k in common] 
+        return sum(good_common)/len(union)    
 
 """cf. https://en.wikipedia.org/wiki/Rank_correlation
 """
 
-def _peak(pk, d=1):
+def _peak(pk, g=1):
     """ Peaksのm/z値を丸め、辞書にして返す
-       実数を整数に丸める。d:桁数 d桁以内四捨五入 
-       例：_fmt(3456.78,d=1) == 3460, _fmt(3456.78,d=2) == 3500
+       実数を整数に丸める。g:桁数 g桁以内四捨五入 
+       例：_fmt(3456.78, g=1) == 3460, _fmt(3456.78, g=2) == 3500
     """
-    npk =  ([round(int(x), -d) for x in pk[0]],pk[1])
+    npk =  ([round(int(x), -g) for x in pk[0]],pk[1])
     return dict(zip(npk[0], npk[1]))
 
 def _rank(pk):
